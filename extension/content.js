3424491a2201820294fc038c8e9a7832f9b5164e
Theriley106 <christopherlambert106@gmail.com>
2019-07-18 17:49:03 -0400

    Added score to the javascript function

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,12 @@
-// this is the code which will be injected into a given page...
-
 (function() {
-	setInterval(function(){ console.log(document.getElementsByClassName("left").innerHTML); }, 1000);
+	setInterval(function(){
+		secondsHTML = document.getElementsByClassName("left")[0].innerHTML;
+		var seconds = parseInt(secondsHTML.match(/\d+/g).map(Number));
+
+		scoreHTML = document.getElementsByClassName("left")[0].innerHTML;
+		var score = parseInt(scoreHTML.match(/\d+/g).map(Number));
+		console.log("Score: ", score);
+		console.log("Seconds: ", seconds);
+
+	}, 1000);
 })();
