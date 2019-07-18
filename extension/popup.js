945b27f79d1783893eb6c9a86e76f63120fed794
Theriley106 <christopherlambert106@gmail.com>
2019-07-18 19:17:47 -0400

    Added popup javascript file

---
+++ extension/popup.js
@@ -0,0 +1,25 @@
+// popup.js
+
+document.body.onload = function() {
+  chrome.storage.sync.get("data", function(items) {
+    if (!chrome.runtime.error) {
+      console.log(items);
+      document.getElementById("data").innerText = "Endpoint: " + items.data;
+    }
+  });
+}
+
+document.getElementById("set").onclick = function() {
+  var d = document.getElementById("text").value;
+  chrome.storage.sync.set({ "data" : d }, function() {
+    if (chrome.runtime.error) {
+      console.log("Runtime error.");
+    }
+  });
+  chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+      var code = 'window.location.reload();';
+      chrome.tabs.executeScript(arrayOfTabs[0].id, {code: code});
+  });
+  window.close();
+
+}
