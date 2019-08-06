10923c79b715adad89df641ed391851ed39e0b9f
theriley106 <christopherlambert106@gmail.com>
2019-08-05 21:23:09 -0400

    Added lambda function

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,16 +1,64 @@
+from __future__ import print_function
+import boto3
+import base64
+from json import loads
+import time
 import json
 
+g = boto3.Session()
+
+
+dynamodb_client = g.resource('dynamodb')
+
+
+def get_id_name():
+	try:
+		# Gets the newest ID name from the DB
+		#dynamodb = session.resource("dynamodb")
+		table = dynamodb.Table("allInventory")
+		#print(table.creation_date_time)
+		response = table.scan()
+		#print response["Items"]
+		return max([x['id'] for x in response["Items"]]) + 1
+	except:
+		return 0
 
 def lambda_handler(event, context):
-	type_of_request = event['httpMethod']
+	#return "WORKING"
+	table = dynamodb_client.Table("zetamac")
+	#print(table.creation_date_time)
+	type_of_request = event.get('httpMethod', "GET")
 	toReturn = {}
+	#return {'idVal':11}
 	if type_of_request == "POST":
-	    information = json.loads(event['body'])
-	    toReturn['finalScore'] = information['score']
-	return {
-		'statusCode': '200',
-		'body': json.dumps(toReturn),
-		'headers': {
-			'Content-Type': 'application/json',
-		},
-	}
+		print("AYY")
+		information = json.loads(event['body'])
+		
+		response = table.scan()
+		try:
+			idVal = max([x['idVal'] for x in response["Items"]]) + 1
+		except:
+			idVal = 0
+		print({'idVal': int(idVal)})
+		table.put_item(
+			Item={"idVal": idVal, "score": information.get("score", None), "time": int(time.time())}
+			)
+		#print('Successfully delivered {0} records, failed to deliver {1} records'.format(success, failure))
+		return {
+			'statusCode': '200',
+			'body': json.dumps({'idVal': int(idVal)}),
+			'headers': {
+				'Content-Type': 'application/json',
+			},
+		}
+	else:
+		a = []
+		for x in table.scan()["Items"]:
+			a.append({"idVal": int(x['idVal']), "score": int(x["score"]), "time": int(x["time"])})
+		return {
+			'statusCode': '200',
+			'body': json.dumps(a),
+			'headers': {
+				'Content-Type': 'application/json',
+			},
+		}
